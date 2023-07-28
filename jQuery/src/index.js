4479fbb0ed55399a8720eb3779dd5fa0387aae3c
Diana Gromova <16adianay@gmail.com>
2023-07-28 18:10:59 +0400

    Added the jQuery example

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,10 +1,48 @@
 $(() => {
-  let count = 0;
-  $('#btn').dxButton({
-    text: `Click count: ${count}`,
-    onClick(e) {
-      count += 1;
-      e.component.option('text', `Click count: ${count}`);
+  let commonConfiguration = {
+    width: 400,
+    itemTemplate(data, index, element) {
+      element.text(data.title)
+    },
+    itemTitleTemplate(data, index, element) {
+      element.text(data.title)
+    },
+    dataSource: tabPanelItems,
+    focusStateEnabled: false,
+    scrollByContent: true,
+    scrollingEnabled: true,
+    showNavButtons: true,
+    deferRendering: true
+  };
+
+  $('#tabpanel1').dxTabPanel({
+    ...commonConfiguration,
+    itemTitleTemplate(data, index, element) {
+      element.text(data.title);
+      element.attr("title", data.title);
+      element.addClass("wordwrap")
     },
   });
+  $('#tabpanel2').dxTabPanel({
+    ...commonConfiguration,
+  });
+  $('#tabpanel3').dxTabPanel({
+    ...commonConfiguration,
+  });
 });
+
+
+const tabPanelItems = [
+  {
+    title: "Lorem ipsum"
+  },
+  {
+    title: "Lorem ipsum dolor sit amet"
+  },
+  {
+    title: "Lorem ipsum dolor sit amet, consectetur"
+  },
+  {
+    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+  }
+];
