a3e41ca038d1154ed5773c175c2e37d45b4a5209
Mark Allen Ramirez <markallenramirez@gmail.com>
2023-09-07 11:24:20 +0800

    TabPanel: Add the Vue example (#4)

--- Vue/src/components/HomeContent.vue
+++ Vue/src/components/HomeContent.vue
@@ -1,22 +1,45 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
-
 import "devextreme/dist/css/dx.material.blue.light.compact.css";
-import DxButton from "devextreme-vue/button";
-
-const props = defineProps({
-  text: String,
-});
-const count = ref(0);
-const buttonText = computed<String>(
-  () => `Click ${props.text}: ${count.value}`
-);
-function clickHandler() {
-  count.value += 1;
-}
+import TabPanelCommon from "./TabPanelCommon.vue";
 </script>
 <template>
   <div>
-    <DxButton :text="buttonText" @click="clickHandler"></DxButton>
+    <p><b>Display ellipses (...)</b></p>
+    <TabPanelCommon
+      class="tabs"
+      item-title-template="ellipsisTitle"
+    >
+      <template #ellipsisTitle="{ data }">
+        <div :title="data.title" class="show-ellipsis">
+          {{ data.title }}
+        </div>
+      </template>
+    </TabPanelCommon>
+
+    <p><b>Show full text</b></p>
+    <TabPanelCommon id="tabpanel2" />
+
+    <p><b>Title word-wrap</b></p>
+    <TabPanelCommon id="tabpanel3" />
   </div>
 </template>
+<style>
+.show-ellipsis {
+  width: 100px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+#tabpanel2 .dx-tabs .dx-tab.dx-item {
+  width: auto;
+}
+
+#tabpanel3 .dx-item.dx-tab {
+  max-width: 200px;
+  white-space: normal;
+}
+
+.tabs {
+  margin-bottom: 30px;
+}
+</style>
